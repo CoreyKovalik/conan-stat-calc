08c5e7fe14232c0851668159290f094b2a540c09
Immotal <coreykovalik@gmail.com>
2018-05-04 17:54:44 -0400

    init commit

---
+++ js/conan-stat-calc.js
@@ -0,0 +1,386 @@
+// Initial Points //
+
+let characterLevel_html = document.getElementById("characterLevel");
+    characterLevel_txt = document.createTextNode(stats.characterLevel);
+characterLevel_html.appendChild(characterLevel_txt);
+
+let unspentPoints_html = document.getElementById("unspentPoints");
+    unspentPoints_txt = document.createTextNode(stats.unspentPoints);
+unspentPoints_html.appendChild(unspentPoints_txt);
+
+let currentExperience_html = document.getElementById("currentExperience");
+    currentExperience_txt = document.createTextNode(stats.currentExperience);
+currentExperience_html.appendChild(currentExperience_txt);
+
+let strength_html = document.getElementById("strength");
+    strength_txt = document.createTextNode(stats.strength.value);
+strength_html.appendChild(strength_txt);
+
+let agility_html = document.getElementById("agility");
+    agility_txt = document.createTextNode(stats.agility.value);
+agility_html.appendChild(agility_txt);
+
+let vitality_html = document.getElementById("vitality");
+    vitality_txt = document.createTextNode(stats.vitality.value);
+vitality_html.appendChild(vitality_txt);
+
+let accuracy_html = document.getElementById("accuracy");
+    accuracy_txt = document.createTextNode(stats.accuracy.value);
+accuracy_html.appendChild(accuracy_txt);
+
+let grit_html = document.getElementById("grit");
+    grit_txt = document.createTextNode(stats.grit.value);
+grit_html.appendChild(grit_txt);
+
+let encumbrance_html = document.getElementById("encumbrance");
+    encumbrance_txt = document.createTextNode(stats.encumbrance.value);
+encumbrance_html.appendChild(encumbrance_txt);
+
+let survival_html = document.getElementById("survival");
+    survival_txt = document.createTextNode(stats.survival.value);
+survival_html.appendChild(survival_txt);
+
+// Initial playerStats //
+
+let health_html = document.getElementById("health");
+    health_txt = document.createTextNode(stats.playerStats.health.value);
+health_html.appendChild(health_txt);
+
+let stamina_html = document.getElementById("stamina");
+    stamina_txt = document.createTextNode(stats.playerStats.stamina.value);
+stamina_html.appendChild(stamina_txt);
+
+let encumbr_player_html = document.getElementById("encumbr-player");
+    encumbr_player_html2 = document.getElementById("encumbr-player-2");
+    encumbr_player_txt = document.createTextNode(stats.playerStats.encumbrance.value);
+    encumbr_player_txt2 = document.createTextNode(stats.playerStats.encumbrance.value);
+encumbr_player_html.appendChild(encumbr_player_txt);
+encumbr_player_html2.appendChild(encumbr_player_txt2);
+
+let melee_html = document.getElementById("melee");
+    melee_txt = document.createTextNode(Math.round(stats.playerStats.melee.value));
+melee_html.appendChild(melee_txt);
+
+let ranged_html = document.getElementById("ranged");
+    ranged_txt = document.createTextNode(Math.round(stats.playerStats.ranged.value));
+ranged_html.appendChild(ranged_txt);
+
+let armor_html = document.getElementById("armor");
+    armor_txt = document.createTextNode(stats.playerStats.armor.value);
+armor_html.appendChild(armor_txt);
+
+let dmg_resist_html = document.getElementById("dmg-resist");
+    dmg_resist_txt = document.createTextNode(precisionRound(stats.playerStats.damageResistance.value, 1) + "%");
+dmg_resist_html.appendChild(dmg_resist_txt);
+
+// Update & adjustment functions //
+
+function update(statString) {
+  adjustPoints();
+  adjustBonuses(statString);
+  calcPlayerStats();
+  adjustPlayerStats();
+  adjustProgress(statString);
+}
+
+function adjustPlayerStats() {
+  health_html.removeChild(health_txt);
+  health_txt = document.createTextNode(stats.playerStats.health.value);
+  health_html.appendChild(health_txt);
+
+  stamina_html.removeChild(stamina_txt);
+  stamina_txt = document.createTextNode(stats.playerStats.stamina.value);
+  stamina_html.appendChild(stamina_txt);
+
+  encumbr_player_html.removeChild(encumbr_player_txt);
+  encumbr_player_txt = document.createTextNode(stats.playerStats.encumbrance.value);
+  encumbr_player_html.appendChild(encumbr_player_txt);
+
+  melee_html.removeChild(melee_txt);
+  melee_txt = document.createTextNode(Math.round(stats.playerStats.melee.value));
+  melee_html.appendChild(melee_txt);
+
+  ranged_html.removeChild(ranged_txt);
+  ranged_txt = document.createTextNode(Math.round(stats.playerStats.ranged.value));
+  ranged_html.appendChild(ranged_txt);
+
+  armor_html.removeChild(armor_txt);
+  armor_txt = document.createTextNode(stats.playerStats.armor.value);
+  armor_html.appendChild(armor_txt);
+
+  dmg_resist_html.removeChild(dmg_resist_txt);
+  dmg_resist_txt = document.createTextNode(precisionRound(stats.playerStats.damageResistance.value, 1) + "%");
+  dmg_resist_html.appendChild(dmg_resist_txt);
+}
+
+function adjustPoints() {
+  stats.lifetimePoints = stats.unspentPoints + stats.spentPoints;
+
+  characterLevel_html.removeChild(characterLevel_txt);
+  characterLevel_txt = document.createTextNode(stats.characterLevel);
+  characterLevel_html.appendChild(characterLevel_txt);
+
+  unspentPoints_html.removeChild(unspentPoints_txt);
+  unspentPoints_txt = document.createTextNode(stats.unspentPoints);
+  unspentPoints_html.appendChild(unspentPoints_txt);
+
+  currentExperience_html.removeChild(currentExperience_txt);
+  currentExperience_txt = document.createTextNode(stats.currentExperience);
+  currentExperience_html.appendChild(currentExperience_txt);
+
+  strength_html.removeChild(strength_txt);
+  strength_txt = document.createTextNode(stats.strength.value);
+  strength_html.appendChild(strength_txt);
+
+  agility_html.removeChild(agility_txt);
+  agility_txt = document.createTextNode(stats.agility.value);
+  agility_html.appendChild(agility_txt);
+
+  vitality_html.removeChild(vitality_txt);
+  vitality_txt = document.createTextNode(stats.vitality.value);
+  vitality_html.appendChild(vitality_txt);
+
+  accuracy_html.removeChild(accuracy_txt);
+  accuracy_txt = document.createTextNode(stats.accuracy.value);
+  accuracy_html.appendChild(accuracy_txt);
+
+  grit_html.removeChild(grit_txt);
+  grit_txt = document.createTextNode(stats.grit.value);
+  grit_html.appendChild(grit_txt);
+
+  encumbrance_html.removeChild(encumbrance_txt);
+  encumbrance_txt = document.createTextNode(stats.encumbrance.value);
+  encumbrance_html.appendChild(encumbrance_txt);
+
+  survival_html.removeChild(survival_txt);
+  survival_txt = document.createTextNode(stats.survival.value);
+  survival_html.appendChild(survival_txt);
+}
+
+// Alert helper function //
+  // alert on point limitations
+
+function limitAlert(event, level) {
+  switch (event) {
+    case "min":
+    alert("Minimum " + level + " level reached!");
+    break;
+    case "max":
+    alert("Maximum " + level + " level reached!");
+    clearInterval(mouseHoldInterval);
+    break;
+    case "nopoints":
+    alert("Not enough points!");
+    break;
+  }
+}
+
+// Increase/Decrease stat functions
+
+function levelUp() {
+  if (stats.characterLevel == 60) return limitAlert("max","exile");
+  stats.characterLevel += 1;
+  setCurrentExperience(stats.characterLevel);
+  stats.unspentPoints += adjustAttrPoints(stats.characterLevel);
+  stats.availableFeats += adjustFeatPoints(stats.characterLevel);
+  update();
+  console.log("Level Up!");
+}
+
+function levelDown() {
+  if (stats.characterLevel == 1) return limitAlert("min","exile");
+  if (stats.unspentPoints < adjustAttrPoints(stats.characterLevel)) {
+    return alert("First remove attributes before leveling down your Exile!  You cannot remove what you've already spent!");
+  }
+  stats.unspentPoints -= adjustAttrPoints(stats.characterLevel);
+  stats.availableFeats -= adjustFeatPoints(stats.characterLevel);
+  stats.characterLevel -= 1;
+  setCurrentExperience(stats.characterLevel);
+  update();
+  console.log("Level Down :(");
+}
+
+function statUp(statString) {
+let stat = stats[statString].value;
+let name = capitalizeFirst(statString);
+let cost = getAttrCost(stat);
+  if (stat == 50) return limitAlert("max", statString);
+  else if (cost > stats.unspentPoints) {
+    return limitAlert("nopoints");
+  }
+  else {
+    stat += 1;
+    stats.unspentPoints -= cost;
+    stats.spentPoints += cost;
+    stats[statString].value = stat;
+    update(statString);
+    console.log(name + " Up!");
+  }
+}
+
+function statDown(statString) {
+  let stat = stats[statString].value;
+  if (stat === 0) return limitAlert("min", statString);
+  else {
+    stat -= 1;
+    let name = capitalizeFirst(statString);
+    let cost = getAttrCost(stat);
+    stats.unspentPoints += cost;
+    stats.spentPoints -= cost;
+    stats[statString].value = stat;
+    update(statString);
+    console.log(name + " down :(");
+  }
+}
+
+// Helper Functions //
+
+let mouseHoldInterval = 0;
+
+function mouseHold(level) {
+  mouseHoldInterval = setInterval(level, 120);
+}
+
+function mouseHoldStatUp(statString) {
+  mouseHoldInterval = setInterval(function() {statUp(statString);}, 120);
+}
+
+function mouseHoldStatDown(statString) {
+  mouseHoldInterval = setInterval(function() {statDown(statString);}, 120);
+}
+
+function mouseReleaseStat() {
+  clearInterval(mouseHoldInterval);
+}
+
+function capitalizeFirst(string) {
+  return string.charAt(0).toUpperCase() + string.substr(1);
+}
+
+function precisionRound(number, precision) {
+  var factor = Math.pow(10, precision);
+  return Math.round(number * factor) / factor;
+}
+
+
+// Apply mouseup/mouseleave event listeners to all stat buttons
+// that clear the mouseHoldInterval for modifying stats
+
+let statButtons = document.getElementsByClassName("stat-button");
+
+for (let button of statButtons) {
+  button.addEventListener("mouseup", mouseReleaseStat);
+  button.addEventListener("mouseleave", mouseReleaseStat);
+}
+
+//Event Listeners on all buttons top to bottom //
+  //character level
+document.getElementById("levelUp").addEventListener("click", levelUp);
+document.getElementById("levelUp").addEventListener("mousedown", function() {
+  mouseHold(levelUp);
+});
+document.getElementById("levelDown").addEventListener("click", levelDown);
+document.getElementById("levelDown").addEventListener("mousedown", function() {
+  mouseHold(levelDown);
+});
+
+  //strength level
+document.getElementById("strUp").addEventListener("click", function() {
+  statUp("strength");
+});
+document.getElementById("strUp").addEventListener("mousedown", function() {
+  mouseHoldStatUp("strength");
+});
+document.getElementById("strDown").addEventListener("click", function() {
+  statDown("strength");
+});
+document.getElementById("strDown").addEventListener("mousedown", function() {
+  mouseHoldStatDown("strength");
+});
+
+  //agility level
+document.getElementById("agiUp").addEventListener("click", function() {
+  statUp("agility");
+});
+document.getElementById("agiUp").addEventListener("mousedown", function() {
+  mouseHoldStatUp("agility");
+});
+document.getElementById("agiDown").addEventListener("click", function() {
+  statDown("agility");
+});
+document.getElementById("agiDown").addEventListener("mousedown", function() {
+  mouseHoldStatDown("agility");
+});
+
+  //vitality
+document.getElementById("vitUp").addEventListener("click", function() {
+  statUp("vitality");
+});
+document.getElementById("vitUp").addEventListener("mousedown", function() {
+  mouseHoldStatUp("vitality");
+});
+document.getElementById("vitDown").addEventListener("click", function() {
+  statDown("vitality");
+});
+document.getElementById("vitDown").addEventListener("mousedown", function() {
+  mouseHoldStatDown("vitality");
+});
+
+  //accuracy
+document.getElementById("accUp").addEventListener("click", function() {
+  statUp("accuracy");
+});
+document.getElementById("accUp").addEventListener("mousedown", function() {
+  mouseHoldStatUp("accuracy");
+});
+document.getElementById("accDown").addEventListener("click", function() {
+  statDown("accuracy");
+});
+document.getElementById("accDown").addEventListener("mousedown", function() {
+  mouseHoldStatDown("accuracy");
+});
+
+  //grit
+document.getElementById("gritUp").addEventListener("click", function() {
+  statUp("grit");
+});
+document.getElementById("gritUp").addEventListener("mousedown", function() {
+  mouseHoldStatUp("grit");
+});
+document.getElementById("gritDown").addEventListener("click", function() {
+  statDown("grit");
+});
+document.getElementById("gritDown").addEventListener("mousedown", function() {
+  mouseHoldStatDown("grit");
+});
+
+  //encumbrance
+document.getElementById("encumbUp").addEventListener("click", function() {
+  statUp("encumbrance");
+});
+document.getElementById("encumbUp").addEventListener("mousedown", function() {
+  mouseHoldStatUp("encumbrance");
+});
+document.getElementById("encumbDown").addEventListener("click", function() {
+  statDown("encumbrance");
+});
+document.getElementById("encumbDown").addEventListener("mousedown", function() {
+  mouseHoldStatDown("encumbrance");
+});
+
+  //survival
+document.getElementById("survUp").addEventListener("click", function() {
+  statUp("survival");
+});
+document.getElementById("survUp").addEventListener("mousedown", function() {
+  mouseHoldStatUp("survival");
+});
+document.getElementById("survDown").addEventListener("click", function() {
+  statDown("survival");
+});
+document.getElementById("survDown").addEventListener("mousedown", function() {
+  mouseHoldStatDown("survival");
+});
+
+//Reset button - all points and levels reverted to base values
+document.getElementById("reset-all").addEventListener("click", resetAll);
---
+++ js/conan-stat-data.js
@@ -0,0 +1,229 @@
+const expArray = [0,275,1325,3675,7825,14325,23675,36400,53000,74000,99925,131300,168625,212450,263275,321600,387975,462900,546900,640475,744175,858500,983975,1121100,1270400,1432400,1607625,1796600,1999825,2217825,2451125,2700225,2965650,3247925,3547575,3865100,4201025,4555875,4930175,5324425,5739150,6174875,6632125,7111400,7613225,8138125,8686600,9259175,9856375,10478725,11126725,11800925,12501825,13229925,13985775,14769875,15582750,16424900,17296850,18199150,19132275];
+
+var stats = {
+  "characterLevel": 1,
+  "unspentPoints": 1,
+  "spentPoints": 0,
+  "lifetimePoints": 1,
+  "availableFeats": 0,
+  "currentExperience": "0 / 275",
+  "allStats": ["strength", "agility", "vitality", "accuracy", "grit", "encumbrance", "survival"],
+  "strength": {
+    "value": 0,
+    "_10": false,
+    "_20": false,
+    "_30": false,
+    "_40": false,
+    "_50": false
+  },
+  "agility": {
+    "value": 0,
+    "_10": false,
+    "_20": false,
+    "_30": false,
+    "_40": false,
+    "_50": false
+  },
+  "vitality": {
+    "value": 0,
+    "_10": false,
+    "_20": false,
+    "_30": false,
+    "_40": false,
+    "_50": false
+  },
+  "accuracy": {
+    "value": 0,
+    "_10": false,
+    "_20": false,
+    "_30": false,
+    "_40": false,
+    "_50": false
+  },
+  "grit": {
+    "value": 0,
+    "_10": false,
+    "_20": false,
+    "_30": false,
+    "_40": false,
+    "_50": false
+  },
+  "encumbrance": {
+    "value": 0,
+    "_10": false,
+    "_20": false,
+    "_30": false,
+    "_40": false,
+    "_50": false
+  },
+  "survival": {
+    "value": 0,
+    "_10": false,
+    "_20": false,
+    "_30": false,
+    "_40": false,
+    "_50": false
+  },
+  "playerStats": {
+    "health": {
+      "value": 200,
+      "base": 200
+    },
+    "stamina": {
+      "value": 100,
+      "base": 100
+    },
+    "encumbrance": {
+      "value": 70,
+      "base": 70
+    },
+    "melee": {
+      "value": 100,
+      "base": 100
+    },
+    "ranged": {
+      "value": 100,
+      "base": 100
+    },
+    "armor": {
+      "value": 0,
+      "base": 0
+    },
+    "damageResistance": {
+      "value": 0,
+      "base": 0
+    }
+  }
+}
+
+function resetAll() {
+  stats.strength.value = stats.agility.value = stats.vitality.value = stats.accuracy.value = stats.grit.value = stats.encumbrance.value = stats.survival.value = stats.spentPoints = stats.availableFeats = 0;
+  stats.characterLevel = stats.unspentPoints = stats.lifetimePoints = 1;
+  stats.currentExperience = "0 / 275";
+  stats.playerStats.health.value = stats.playerStats.health.base;
+  stats.playerStats.stamina.value = stats.playerStats.melee.value = stats.playerStats.ranged.value = stats.playerStats.stamina.base;
+  stats.playerStats.encumbrance.value = stats.playerStats.encumbrance.base;
+  stats.playerStats.armor.value = stats.playerStats.damageResistance.value = stats.playerStats.armor.base;
+  for (var i = 0; i < 7; i++) {
+    update(stats.allStats[i]);
+  }
+  alert("All levels, stats, and points are reset.");
+}
+
+function setCurrentExperience(currentlevel) {
+  if (currentlevel < 1 || currentlevel > 60) {
+    return;
+  }
+  stats.currentExperience = expArray[currentlevel - 1].toLocaleString() + " / " + expArray[currentlevel].toLocaleString();
+  return expArray[currentlevel -1];
+}
+
+function getAttrCost(currentlevel) {
+  if (currentlevel < 5) return 1;
+  else if (currentlevel < 10) return 2;
+  else if (currentlevel < 15) return 3;
+  else if (currentlevel < 20) return 4;
+  else if (currentlevel < 25) return 5;
+  else if (currentlevel < 30) return 6;
+  else if (currentlevel < 35) return 7;
+  else if (currentlevel < 40) return 8;
+  else if (currentlevel < 45) return 9;
+  else if (currentlevel < 50) return 10;
+  else return null;
+}
+
+function adjustAttrPoints(currentlevel) {
+  if (currentlevel <= 5) return 1;
+  else if (currentlevel <= 10) return 2;
+  else if (currentlevel <= 15) return 3;
+  else if (currentlevel <= 20) return 4;
+  else if (currentlevel <= 25) return 5;
+  else if (currentlevel <= 30) return 6;
+  else if (currentlevel <= 35) return 7;
+  else if (currentlevel <= 40) return 8;
+  else if (currentlevel <= 45) return 9;
+  else if (currentlevel <= 50) return 10;
+  else if (currentlevel <= 55) return 11;
+  else if (currentlevel <= 60) return 12;
+  else return null;
+  // let i = 0;
+  // let comparelvl = 0;
+  // let attrPoints = 0;
+  // while (i < 13) {
+  //   if (currentlevel <= comparelvl + 5) return attrPoints + 1;
+  //   else i++, attrPoints++, comparelvl += 5;
+  // }
+}
+
+function adjustFeatPoints(currentlevel) {
+  if (currentlevel == 1) {
+    // stats.availableFeats = 0;
+    return null;
+  }
+  else if (currentlevel == 10) return 9;
+  else if (currentlevel == 20) return 15;
+  else if (currentlevel == 30) return 21;
+  else if (currentlevel == 40) return 27;
+  else if (currentlevel == 50) return 33;
+  else if (currentlevel == 60) return 39;
+  else if (currentlevel < 5) return 1;
+  else if (currentlevel < 10) return 2;
+  else if (currentlevel < 15) return 3;
+  else if (currentlevel < 20) return 4;
+  else if (currentlevel < 25) return 5;
+  else if (currentlevel < 30) return 6;
+  else if (currentlevel < 35) return 7;
+  else if (currentlevel < 40) return 8;
+  else if (currentlevel < 45) return 9;
+  else if (currentlevel < 50) return 10;
+  else if (currentlevel < 55) return 11;
+  else if (currentlevel < 60) return 12;
+  else return null;
+
+//   let i = 0;
+//   let comparelvl = 0;
+//   let featPoints = 0;
+//   while (i < 13) {
+//     if (currentlevel < comparelvl + 5) return featPoints + 1;
+//     else if
+//     else i++, featPoints++, comparelvl += 5;
+//   }
+}
+
+//Checks current attributes and determines attribute bonuses @ lvls 10,20,30,40,50
+
+function adjustBonuses(statString) {
+  if (statString == null) return false;
+    for (var i = 1; i <= 5; i++) {
+    let teir = "_" + i + "0";
+    let lvl = i * 10;
+    stats[statString].value >= lvl ? stats[statString][teir] = true : stats[statString][teir] = false;
+  }
+}
+
+//Math calculations for playerStats based on attributes and certain bonus perks
+
+function calcPlayerStats() {
+  stats.playerStats.health.value = (8 * stats.vitality.value) + stats.playerStats.health.base;
+  stats.playerStats.stamina.value = (3 * stats.grit.value) + stats.playerStats.stamina.base;
+  stats.playerStats.encumbrance.value = (7 * stats.encumbrance.value) + stats.playerStats.encumbrance.base;
+  stats.playerStats.melee.value = (100 * 0.025 * stats.strength.value) + stats.playerStats.melee.base;
+  stats.playerStats.ranged.value = (100 * 0.025 * stats.accuracy.value) + stats.playerStats.ranged.base;
+    if(stats.accuracy._30) stats.playerStats.ranged.value += 10;
+  stats.playerStats.armor.value = (2 * stats.agility.value) + stats.playerStats.armor.base;
+    if(stats.grit._30) stats.playerStats.armor.value += 15;
+  stats.playerStats.damageResistance.value = stats.playerStats.armor.value * 0.003 * 100;
+}
+
+function adjustProgress(statString) {
+  if (statString == null) return false;
+
+  let statProgress = (stats[statString].value / 50) * 100;
+  document.getElementById("progress-" + statString).setAttribute("style", "width:" + statProgress + "%;");
+
+  for (var i = 1; i <= 5; i++) {
+    let teir = "_" + i + "0";
+    if (stats[statString][teir]) document.getElementById("progress-bar-" + statString).classList.add("perk-" + i);
+    else document.getElementById("progress-bar-" + statString).classList.remove("perk-" + i);
+  }
+}
